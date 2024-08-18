f47c820588648f8cc95ff3b9be89a49f9471fa0e
kmacakai <karina.macakaite@frasers.group>
2024-08-18 20:11:01 +0100

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 // await inbound HTTP requests
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { abortOnError: false }); // abortOnError to throw an error instead of exiting app
-  await app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+  app.enableCors()
+  await app.listen(4000, () => console.log('Server is running on http://localhost:4000'));
 }
 bootstrap();
